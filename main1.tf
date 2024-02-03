459c219c3248c311dbcdc663bddce89183fc9c57
bisirinivas <127945511+bisirinivas@users.noreply.github.com>
2024-02-03 09:27:04 +0000

    Create main1.tf
    addded vm,sybnets,nsg,vm

---
+++ main1.tf
@@ -0,0 +1,139 @@
+# 1. Specify the version of the AzureRM Provider to use
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0.0"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "GPA-Prod"
+    storage_account_name = "tfstatestorageaccount34"
+    container_name       = "tfstatecontainer"
+    key                  = "terraform.tfstate"
+    access_key           = "xxxxx=="
+  }
+}
+
+provider "azurerm" {
+  features {}
+  client_id       = "xxx"
+  client_secret   = "xxx"
+  tenant_id       = "xxx"
+  subscription_id = "xxx"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "GPA-Prod"
+  location = "UK South"
+
+  tags = {
+    Ram = "Ram"
+  }
+
+}
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstatestorageaccount34"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_container" "tfstatecontainer" {
+  name                  = "tfstatecontainer"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "blob"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "GPA-Vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/21"]
+
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "GPA-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "GPA-subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+resource "azurerm_network_security_group" "sunenetNSG" {
+  name = "nsgforsubnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  
+  
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsgassociate" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.sunenetNSG.id
+}
+
+
+resource "azurerm_network_interface" "nic" {
+  name                = "GPA-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "GPA-vmmachine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
